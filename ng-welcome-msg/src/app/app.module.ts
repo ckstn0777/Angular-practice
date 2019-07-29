274d03daaf0940b4b8c77198e38d09b7ddb9b183
ckstn0777 <ckstn0777@naver.com>
2019-07-29 18:05:20 +0900

    add ng-welcome-msg

---
+++ ng-welcome-msg/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, COMPOSITION_BUFFER_MODE } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { WelcomeMsgComponent } from './welcome-msg/welcome-msg.component';
+import { LangSelectorComponent } from './lang-selector/lang-selector.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WelcomeMsgComponent,
+    LangSelectorComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+  ],
+  providers: [{provide:COMPOSITION_BUFFER_MODE,useValue:false}],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
